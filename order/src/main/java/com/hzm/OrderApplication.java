c064f6b1c8eb518f78226fcc9705a7a140cb4b3d
huangzhenmu <946624404@qq.com>
2019-11-12 00:23:26 +0800

    添加配置中心和注册中心以及order服务提供者三个服务

---
+++ order/src/main/java/com/hzm/OrderApplication.java
@@ -0,0 +1,12 @@
+package com.hzm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.annotation.Order;
+
+@SpringBootApplication
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class,args);
+    }
+}
---
+++ order/src/main/java/com/hzm/controller/OrdeHandler.java
@@ -0,0 +1,18 @@
+package com.hzm.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrdeHandler {
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/index")
+    public String index(){
+        return "order端口:"+this.port;
+    }
+}
