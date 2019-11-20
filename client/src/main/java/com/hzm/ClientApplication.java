d3157fcbf67fadeae34129b0d75601a0d2f8de34
huangzhenmu <huangzhenmu@pearlgo.com>
2019-11-20 23:59:06 +0800

    client启动类添加注释扫描过滤类

--- client/src/main/java/com/hzm/ClientApplication.java
+++ client/src/main/java/com/hzm/ClientApplication.java
@@ -2,10 +2,12 @@ package com.hzm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients//会帮助我们创建@FeignClient的接口的实现类，不然用不了
+@ServletComponentScan//扫描组件，这样才能使拦截器有效
 public class ClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class,args);
