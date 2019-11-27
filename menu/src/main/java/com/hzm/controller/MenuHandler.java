e5b42f76ee8726ab7ae28e1372ca12fb82d9f30b
huangzhenmu <946624404@qq.com>
2019-11-27 16:20:37 +0800

    menu添加swagger-ui工具在线api文档

--- menu/src/main/java/com/hzm/controller/MenuHandler.java
+++ menu/src/main/java/com/hzm/controller/MenuHandler.java
@@ -4,6 +4,9 @@ import com.hzm.entity.Menu;
 import com.hzm.entity.MenuVo;
 import com.hzm.entity.Type;
 import com.hzm.services.impl.MenuServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/menu")
+@Api(tags = "MenuHandler", description = "菜单管理")
 public class MenuHandler {
     @Value("${server.port}")
     private String port;
@@ -24,7 +28,8 @@ public class MenuHandler {
     }
 
     @GetMapping("/findAll/{index}/{limit}")
-    public MenuVo findAll(@PathVariable("index") int index, @PathVariable("limit") int limit){
+    @ApiOperation("获取所有菜品列表")
+    public MenuVo findAll(@PathVariable("index")@ApiParam("起点") int index, @PathVariable("limit")@ApiParam("范围") int limit){
         List<Menu> list = menuService.findAll(index, limit);
         return new MenuVo(200,"成功",menuService.count(),list);
     }
